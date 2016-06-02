7956e44cb37a2863defa9b0b9d32ccf1cb7c3c24
Michael Moosbauer <m.moosbauer@fupa.net>
2016-06-02 07:55:53 +0200

    maptest: inital commit

---
+++ maptest/Googlemap/app/src/main/java/com/example/marwan/googlemap/MapsActivity.java
@@ -0,0 +1,54 @@
+package com.example.marwan.googlemap;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //Add a marker in Passau and move the camera
+
+        LatLng Passau = new LatLng(48.569957, 13.457025);
+
+        mMap.addMarker(new MarkerOptions().position(Passau).title("Chuckhole 1"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Passau,13));
+
+
+
+    }
+}
