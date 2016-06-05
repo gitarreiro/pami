68b45d43cb256eed1043f61414db7e9f00d5ceb2
Marwan <marwanelmezni@hotmail.com>
2016-06-05 17:05:17 +0200

    added basic map

--- mainapp/MissionChuckhole/app/src/main/java/mainapp/mimomusic/de/missionchuckhole/activity/MapActivity.java
+++ mainapp/MissionChuckhole/app/src/main/java/mainapp/mimomusic/de/missionchuckhole/activity/MapActivity.java
@@ -1,9 +1,98 @@
 package mainapp.mimomusic.de.missionchuckhole.activity;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 
-/**
- * Created by MiMo
- */
-public class MapActivity extends FragmentActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.Gradient;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mainapp.mimomusic.de.missionchuckhole.R;
+
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        //Add a marker in Passau and move the camera
+
+        LatLng Passau = new LatLng(48.569303, 13.440118);
+
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Passau,13));
+
+        List<LatLng> list = new ArrayList<>();
+
+        list.add(new LatLng(48.569635,13.437468));
+        list.add(new LatLng(48.570099,13.439429));
+        list.add(new LatLng(48.569276,13.439373));
+        list.add(new LatLng(48.568917,13.440532));
+        list.add(new LatLng(48.568914,13.442915));
+
+
+
+
+        int[] colors = {
+                Color.rgb(102, 225, 0), // green
+                Color.rgb(255, 0, 0)    // red
+        };
+
+        float[] startPoints = {
+                0.2f, 1f
+        };
+
+        Gradient gradient = new Gradient(colors, startPoints);
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+        mProvider = new HeatmapTileProvider.Builder()
+                .data(list)
+                .gradient(gradient)
+                .build();
+        // Add a tile overlay to the map, using the heat map tile provider.
+        mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+
+    }
+
+
+
 }
