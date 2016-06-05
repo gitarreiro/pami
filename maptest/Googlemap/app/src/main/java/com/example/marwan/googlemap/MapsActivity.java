40b25b1a487d182ecb5f95a4886bde03b40f03fd
Marwan <marwanelmezni@hotmail.com>
2016-06-05 16:08:22 +0200

    some changes

--- maptest/Googlemap/app/src/main/java/com/example/marwan/googlemap/MapsActivity.java
+++ maptest/Googlemap/app/src/main/java/com/example/marwan/googlemap/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.example.marwan.googlemap;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -8,13 +9,24 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.Gradient;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +48,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
         //Add a marker in Passau and move the camera
 
-        LatLng Passau = new LatLng(48.569957, 13.457025);
+        LatLng Passau = new LatLng(48.569303, 13.440118);
 
-        mMap.addMarker(new MarkerOptions().position(Passau).title("Chuckhole 1"));
+
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Passau,13));
 
+        List<LatLng> list = new ArrayList<>();
 
+        list.add(new LatLng(48.569635,13.437468));
+        list.add(new LatLng(48.570099,13.439429));
+        list.add(new LatLng(48.569276,13.439373));
+        list.add(new LatLng(48.568917,13.440532));
+        list.add(new LatLng(48.568914,13.442915));
+
+
+
+
+        int[] colors = {
+                Color.rgb(102, 225, 0), // green
+                Color.rgb(255, 0, 0)    // red
+        };
+
+        float[] startPoints = {
+                0.2f, 1f
+        };
+
+        Gradient gradient = new Gradient(colors, startPoints);
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+        mProvider = new HeatmapTileProvider.Builder()
+                .data(list)
+                .gradient(gradient)
+                .build();
+        // Add a tile overlay to the map, using the heat map tile provider.
+        mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
 
     }
+
+
+
 }
