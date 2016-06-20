1735fe4dd0da78364d0e16e244431c01b21b92b6
Marwan <marwanelmezni@hotmail.com>
2016-06-21 01:55:59 +0200

    - MyItem Java class necessary for clustering the markers of the chuckholes. - update of the xml file of the map activity.

---
+++ mainapp/MissionChuckhole/app/src/main/java/mainapp/mimomusic/de/missionchuckhole/data/MyItem.java
@@ -0,0 +1,20 @@
+package mainapp.mimomusic.de.missionchuckhole.data;
+
+        import com.google.android.gms.maps.model.LatLng;
+        import com.google.maps.android.clustering.ClusterItem;
+
+/*
+ Created by Marwan on 18/06/2016.
+ */
+public class MyItem implements ClusterItem {
+    private final LatLng mPosition;
+
+    public MyItem(double lat, double lng) {
+        mPosition = new LatLng(lat, lng);
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+}
