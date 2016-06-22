a4b429314061a19c968f51773eef00184655132b
Marwan <marwanelmezni@hotmail.com>
2016-06-23 01:43:30 +0200

    - Radio buttons for the map : dynamic heatmaps and clustering markers on chuckholes. - Current location of the user detected automatically by the app. - Acquiring/Requesting user permissions to access GPS localization. - Better feedback for the user.

--- mainapp/MissionChuckhole/app/src/main/java/mainapp/mimomusic/de/missionchuckhole/activity/MapActivity.java
+++ mainapp/MissionChuckhole/app/src/main/java/mainapp/mimomusic/de/missionchuckhole/activity/MapActivity.java
@@ -3,16 +3,22 @@ package mainapp.mimomusic.de.missionchuckhole.activity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.TileOverlay;
@@ -35,13 +41,13 @@ import mainapp.mimomusic.de.missionchuckhole.data.MyItem;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private HeatmapTileProvider mProvider1, mProvider2, mProvider3;
+    private HeatmapTileProvider mProvider1;
     private TileOverlay mOverlay;
     private List<AccFix> records = new ArrayList<>();
-    ClusterManager<MyItem> mClusterManager;
+    private ClusterManager<MyItem> mClusterManager;
     double lat, lng, intensity;
-    Location L;
-
+    private Location L;
+    boolean verif1,verif2;
 
 
     @Override
@@ -51,6 +57,41 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+
+        final RadioButton heatMaps = (RadioButton) findViewById(R.id.radioButton1);
+        final RadioButton markers = (RadioButton) findViewById(R.id.radioButton2);
+
+
+        heatMaps.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (heatMaps.isChecked()) {
+                    markers.setChecked(false);
+                    if (verif1)
+                    mClusterManager.clearItems();
+
+                    dynamic_heatmap();
+
+
+
+                }
+            }
+        });
+
+        markers.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (markers.isChecked())
+                {
+                    heatMaps.setChecked(false);
+                    if (verif2)
+                    mOverlay.remove();
+
+                    markers_clustering();
+                }
+            }
+        });
 
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -72,35 +113,45 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
      */
 
 
+    public void onMapReady(GoogleMap googleMap) {
 
-    public void onMapReady(GoogleMap googleMap)
-    {
         mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        //mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-        } else {
-            System.out.println("permission required");
+
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            String provider = locationManager.getBestProvider(criteria, true);
+            Location myLocation = locationManager.getLastKnownLocation(provider);
+            double lat = myLocation.getLatitude();
+            double lng = myLocation.getLongitude();
+            LatLng latLng = new LatLng(lat, lng);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
         }
-
-
-       //markers_clustering();
-       dynamic_heatmap();
-       //static_heatmap();
+        else
+        {
+            // request permission
+            String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissions, 0);
+        }
 
 
     }
 
-     private void markers_clustering ()
-    {
-
-        // Position the map.
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(48.719372, 13.383121), 14));
-
+    private void markers_clustering() {
         // Initialize the manager with the context and the map.
         // (Activity extends context, so we can pass 'this' in the constructor.)
-        mClusterManager = new ClusterManager <MyItem>(this, mMap);
+        verif1 = true;
+        mClusterManager = new ClusterManager<MyItem>(this, mMap);
 
         // Point the map's listeners at the listeners implemented by the cluster manager.
         mMap.setOnCameraChangeListener(mClusterManager);
@@ -108,16 +159,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
         // Add cluster items (markers) to the cluster manager.
 
-       records = DataStore.getInstance(this).getFixes();
+        records = DataStore.getInstance(this).getFixes();
 
         for (AccFix record : records) {
 
             L = record.getLocation();
 
-            intensity = record.getgForce() / 5.0;
-            System.out.println(intensity);
-            if (intensity>=0.233)
-            {
+
+            if (record.isBigChuckhole()) {
                 lat = L.getLatitude();
                 lng = L.getLongitude();
                 MyItem offsetItem = new MyItem(lat, lng);
@@ -126,49 +175,23 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             }
 
 
-        }
 
+        }
+        if (mClusterManager.getMarkerCollection().getMarkers().size()==0)
+            Toast.makeText(this, "no chuckholes were detected", Toast.LENGTH_SHORT).show();
     }
 
-    private void dynamic_heatmap ()
+    private void dynamic_heatmap()
 
     {
-        //float zoomLevel = mMap.getCameraPosition().zoom;
 
-        LatLng Tittling = new LatLng(48.727804, 13.382363);
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Tittling,12));
-
-        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
-            final int MaxZoom =13, MinZoom=9;
-            double PreviousZoom= -1.0;
-
-            @Override
-            public void onCameraChange(CameraPosition position)
-            {
-                if (position.zoom<= MaxZoom && position.zoom>= MinZoom)
-                {
-                    System.out.println("clustering is used");
-                    //mOverlay.remove();
-                    records = DataStore.getInstance(getApplicationContext()).getFixes2();
-                    overlay(records);
-                }
-                else
-                {
-                    System.out.println("no clustering is used");
-                    //mOverlay.remove();
-                    records = DataStore.getInstance(getApplicationContext()).getFixes();
-                    overlay(records);
-
-                }
-
-            }
-        });
-
+        verif2 = true;
+        records = DataStore.getInstance(getApplicationContext()).getFixes();
+        overlay(records);
 
     }
 
-    private void overlay (List<AccFix> r)
+    private void overlay(List<AccFix> r)
 
     {
 
@@ -182,8 +205,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             lng = L.getLongitude();
             //System.out.println(record.getgForce());
             intensity = record.getgForce() / 5.0;
-            if (intensity >=1)
-                intensity =1;
+            if (intensity >= 1)
+                intensity = 1;
 
             WeightedLatLng detection = new WeightedLatLng(new LatLng(lat, lng), intensity);
 
@@ -205,24 +228,30 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
         Gradient gradient1 = new Gradient(Colors1, StartPoints);
 
-        if (list.size()==0)
+        if (list.size() == 0)
             Toast.makeText(this, "inexistent dataset", Toast.LENGTH_SHORT).show();
         else
         {
-            mProvider1 = new HeatmapTileProvider.Builder()
-                    .weightedData(list)
-                    .radius(10)
-                    //.gradient(gradient1)
-                    .build();
+            //if (mProvider1 == null)
+            //{
+                mProvider1 = new HeatmapTileProvider.Builder()
+                        .weightedData(list)
+                        .radius(10)
+                        //.gradient(gradient1)
+                        .build();
+
+                //mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider1));
 
 
-            mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider1));
+            //mProvider1.setWeightedData(list);
+            //mOverlay.clearTileCache();
+
+            //if (mOverlay == null) {
+                mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider1));
+           // }
+
         }
-
-
     }
-
-
 /*
     private void static_heatmap ()
     {
@@ -308,85 +337,8 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
     }
 
-*/
+
+
+   */
 
 }
-/*
-
-list.add(new WeightedLatLng(new LatLng(48.727660, 13.383032),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.727528, 13.383125),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.727793, 13.382612),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.727450, 13.383182),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.727365, 13.383254),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.727268, 13.383351),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.727170, 13.383473),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.727067, 13.383605),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.726934, 13.383785),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.726837, 13.383927),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.726728, 13.384087),0.5));
-
-        list.add(new WeightedLatLng(new LatLng(48.726588, 13.384279),1));
-
-        list.add(new WeightedLatLng(new LatLng(48.726456, 13.384388),1));
-
-        list.add(new WeightedLatLng(new LatLng(48.726319, 13.384443),0.2));
-
-        list.add(new WeightedLatLng(new LatLng(48.726219, 13.384484),0.2));
-
-        list.add(new WeightedLatLng(new LatLng(48.726101, 13.384533),1));
-
-        list.add(new WeightedLatLng(new LatLng(48.725958, 13.384572),1));
-
-        list.add(new WeightedLatLng(new LatLng(48.725584, 13.384679),0.2));
-
-        ------------------------------------------------------------------
-
-
-        list1.add(new LatLng(48.727660, 13.383032));
-
-        list1.add(new LatLng(48.727793, 13.382612));
-
-        list1.add(new LatLng(48.727450, 13.383182));
-
-        list1.add(new LatLng(48.727365, 13.383254));
-
-        list2.add(new LatLng(48.727268, 13.383351));
-
-        list2.add(new LatLng(48.727067, 13.383605));
-
-        list2.add(new LatLng(48.726934, 13.383785));
-
-        list2.add(new LatLng(48.726837, 13.383927));
-
-        list2.add(new LatLng(48.726728, 13.384087));
-
-        list3.add(new LatLng(48.726588, 13.384279));
-
-        list3.add(new LatLng(48.726456, 13.384388));
-
-        list3.add(new LatLng(48.726319, 13.384443));
-
-        list3.add(new LatLng(48.726219, 13.384484));
-
-        list2.add(new LatLng(48.726101, 13.384533));
-
-        list1.add(new LatLng(48.725958, 13.384572));
-
-        list1.add(new LatLng(48.725584, 13.384679));
-
-
-
-
-
-
- */
